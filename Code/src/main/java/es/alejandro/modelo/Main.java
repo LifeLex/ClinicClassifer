dc3f11e9a5ff7293a869c6a23eef08170852784a
Alejandro <jandrito97@gmail.com>
2019-09-08 20:50:07 +0200

    Update

--- Code/src/main/java/es/alejandro/modelo/Main.java
+++ Code/src/main/java/es/alejandro/modelo/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("/es/alejandro/interfaces/sample.fxml"));
         Scene scene = new Scene(root);
 
         primaryStage.setScene(scene);
